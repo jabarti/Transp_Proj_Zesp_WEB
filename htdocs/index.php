a7fee92023566be14b5526eb3f8267a576547bd9
Bartosz Lewinski <jabarti@wp.pl>
2014-03-28 23:52:46 +0100

    save name commits

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * commit e5933246f001d0f4d742bc7e4f3ec581fc34d32d
- * Date:   Fri Mar 28 17:44:24 2014 +0100
+ * commit       e6f329a919e0bf91cd35513d51722adfedeb18c0  fixing1
+ * Date:        Fri Mar 28 23:42:59 2014 +0100
  */
 require_once "common.inc.php";
 //require_once HDD_STABLEVIEWS_PATH.'header.php';
--- htdocs/scripts/Valid_MakeLoginPanel.js
+++ htdocs/scripts/Valid_MakeLoginPanel.js
@@ -4,8 +4,8 @@
  *  Created:    2014-01-14
  * 
  *  @author     Bartosz M. Lewiński <jabarti@wp.pl>
- * commit e5933246f001d0f4d742bc7e4f3ec581fc34d32d
- * Date:   Fri Mar 28 17:44:24 2014 +0100
+ *  commit       e6f329a919e0bf91cd35513d51722adfedeb18c0  fixing1
+ *  Date:        Fri Mar 28 23:42:59 2014 +0100
  ***************************************************/
 window.onload = 											/*		REJESTRACJA FIRMA	 	*/
 function (){
